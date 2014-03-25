c449aff3c77018159ba41feffa1a5c8756e9e94c
Rebecca Meritz <rebecca@meritz.com>
2014-03-25 12:53:42 +0100

    Add Mangopay client

---
+++ mangopay/client.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+from mangopaysdk.mangopayapi import MangoPayApi
+from mangopaysdk.tools.storages.memorystoragestrategy import (
+    MemoryStorageStrategy)
+
+
+def get_mangopay_api_client():
+    api = MangoPayApi()
+    api.OAuthTokenManager.RegisterCustomStorageStrategy(
+        MemoryStorageStrategy())
+    api.Config.ClientID = settings.MANGOPAY_CLIENT_ID
+    api.Config.ClientPassword = settings.MANGOPAY_PASSPHRASE
+    api.Config.DebugMode = settings.MANGOPAY_DEBUG_MODE
+    return api
