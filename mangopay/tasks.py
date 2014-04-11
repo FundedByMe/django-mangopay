8ced542b9577918c9d2f450872a06b5539e23c4f
Rebecca Meritz <rebecca@meritz.com>
2014-04-11 15:02:50 +0200

    Add Document check task

--- mangopay/tasks.py
+++ mangopay/tasks.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+
 from celery.task import task
 
+from .constants import VALIDATION_ASKED
 from .models import (MangoPayUser, MangoPayBankAccount,
                      MangoPayDocument, MangoPayWallet, MangoPayPayOut)
 
@@ -29,6 +32,24 @@ def create_mangopay_document_and_page_and_ask_for_validation(id):
     document.create()
     document.create_page()
     document.ask_for_validation()
+
+
+def next_weekday():
+    def maybe_add_day(date):
+        if datetime.weekday(date) > 6:
+            date = date + timedelta(days=1)
+            return maybe_add_day(date)
+        else:
+            return date
+
+
+@task
+def update_document_status(id):
+    document = MangoPayDocument.objects.get(id=id, status=VALIDATION_ASKED)
+    updated_document = document.get()
+    if updated_document.status == VALIDATION_ASKED:
+        eta = next_weekday()
+        update_document_status.apply_async({id: id}, eta=eta)
 
 
 @task
