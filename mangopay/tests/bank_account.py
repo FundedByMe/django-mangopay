0a75421dbaf32f5e38f764fd081efb5bf2d407b4
Flavia Missi <flaviamissi@gmail.com>
2015-11-03 09:54:24 +0100

    mangopay.models: use unicode for mangopay user when creating bank account

--- mangopay/tests/bank_account.py
+++ mangopay/tests/bank_account.py
@@ -11,7 +11,6 @@ from .client import MockMangoPayApi
 
 from ..constants import (BA_BIC_IBAN,
                          BA_US,
-                         BA_NOT_IMPLEMENTED,
                          BA_OTHER)
 
 
@@ -65,3 +64,20 @@ class MangoPayBankAccountTests(TestCase):
 
         self.assertIsNone(self.bank_account_other.iban)
         self.assertIsNotNone(self.bank_account_other.account_number)
+
+    @patch("mangopay.models.get_mangopay_api_client")
+    def test_create_with_unicode_characters(self, mock_client):
+        id_ = 33322
+        mock_client.return_value = MockMangoPayApi(bank_account_id=id_)
+        self.assertIsNone(self.bank_account_iban.mangopay_id)
+        self.bank_account_iban.address = u"Sveavägen 41"
+        self.bank_account_iban.save()
+
+        self.bank_account_iban.create()
+        MangoPayBankAccount.objects.get(id=self.bank_account_iban.id,
+                                        mangopay_id=id_)
+        self.assertEqual(self.bank_account_iban.account_type,
+                         BA_BIC_IBAN)
+
+        self.assertIsNone(self.bank_account_iban.account_number)
+        self.assertIsNotNone(self.bank_account_iban.iban)
